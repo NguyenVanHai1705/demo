92141293077df661366ddfdb915de820d73e21b8
Your Name <you@example.com>
2022-03-18 10:52:03 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React ,{useState } from 'react';
+import { render } from 'react-dom';
+import LoginForm from './component/LoginForm';
+
+
+function App() {
+  const adminUser={
+    email:"hai@admin.com",
+    password :"hai123"
+  }
+  const [user, setUser]= useState({name:"",email:""});
+  const [error,setError]=useState("");
+
+  const Login = details =>{
+    console.log(details);
+    if (details.email==adminUser.email && details.password == adminUser.password){
+      console.log("Logged in");   
+      setUser({
+        name: details.name,
+        email: details.email
+      });
+    }else{
+    console.log("Details do not match!");
+      setError("Details do not match!");
+  }
+}
+
+  const Logout =() =>{
+    setUser({name:"",email:""});
+  }
+  return (
+    <div className="App">
+      {(user.email !="")?(
+        <div className="Welcome">
+        <h2>Welcome, <span>{user.name}</span></h2>
+        <button onClick={Logout}>Logout</button>
+       </div>
+      
+  ):(
+    <LoginForm Login={Login} error={error} />
+
+  )}
+
+  </div>
+  )}
+  export default App;
